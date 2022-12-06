0867113c5ea8693b37f2d0094cc6a3501305a6b7
Colin Boardway <cmboardway@gmail.com>
2022-12-06 14:16:53 -0600

    refactor registration page to use universal form component

---
+++ src/components/Form/FormRegister.tsx
@@ -0,0 +1,103 @@
+import type { ReactNode } from "react";
+import { useEffect, useState } from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+// hooks
+import useAuth from "hooks/useAuth";
+
+// components
+import { IonButton } from "@ionic/react";
+import { FormInput } from "components/Form/FormInput";
+
+// helpers
+import {
+  firstNameValidation,
+  lastNameValidation,
+  emailValidation,
+  phoneValidation,
+  passwordValidation,
+} from "helpers/formValidation";
+
+// styles
+import "components/Form/Form.scss";
+
+export default function FormRegister() {
+  const { error, register } = useAuth();
+  const [, setErrorMessage] = useState<ReactNode>(null);
+
+  // Not correct
+  // Will improve after Jairo's update is merged
+  useEffect(() => {
+    error &&
+      setErrorMessage(
+        <p>
+          A link to activate your account has been emailed to the address
+          provided.
+        </p>
+      );
+  }, [error]);
+
+  return (
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+        password: "",
+      }}
+      validationSchema={Yup.object({
+        firstName: firstNameValidation,
+        lastName: lastNameValidation,
+        email: emailValidation,
+        phone: phoneValidation,
+        password: passwordValidation,
+      })}
+      onSubmit={(values, actions) => {
+        const vals = { ...values };
+        actions.resetForm();
+        register(vals);
+      }}
+    >
+      {(formik) => (
+        <form onSubmit={formik.handleSubmit}>
+          <FormInput
+            label="First Name"
+            name="firstName"
+            type="text"
+            placeholder="Jane"
+          />
+          <FormInput
+            label="Last Name"
+            name="lastName"
+            type="text"
+            placeholder="First Name"
+          />
+          <div className="Form__inputGroup">
+            <FormInput
+              label="Email"
+              name="email"
+              type="email"
+              placeholder="Email"
+            />
+            <FormInput
+              label="Phone"
+              name="phone"
+              type="text"
+              placeholder="Phone"
+            />
+            <FormInput
+              label="Password"
+              name="password"
+              type="password"
+              placeholder="Password"
+            />
+          </div>
+
+          <IonButton type="submit">Register</IonButton>
+        </form>
+      )}
+    </Formik>
+  );
+}
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,135 +1,24 @@
-import type { ReactNode } from "react";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { Formik } from "formik";
-import * as Yup from "yup";
-
-// hooks
-import useAuth from "hooks/useAuth";
-
-// components
-import { IonButton, IonContent, IonIcon, IonPage, IonText } from "@ionic/react";
-import Logo from "components/Logo/Logo";
-import { closeOutline } from "ionicons/icons";
-import { FormInput } from "components/Form/FormInput";
-
-// helpers
-import {
-  firstNameValidation,
-  lastNameValidation,
-  emailValidation,
-  phoneValidation,
-  passwordValidation,
-} from "helpers/formValidation";
-
-// styles
-import "components/Form/Form.scss";
+import Form from "components/Form/Form";
+import FormRegister from "components/Form/FormRegister";
 
 export default function Register() {
-  const { error, register } = useAuth();
-  const [errorMessage, setErrorMessage] = useState<ReactNode>(null);
-
-  // Not correct
-  // Will improve after Jairo's update is merged
-  useEffect(() => {
-    error &&
-      setErrorMessage(
-        <p>
-          A link to activate your account has been emailed to the address
-          provided.
-        </p>
-      );
-  }, [error]);
-
   return (
-    <IonPage className="Form">
-      <IonContent fullscreen className="ion-padding">
-        <Logo />
-        <div>
-          <IonText>
-            <h1>Sign Up</h1>
-          </IonText>
-          {errorMessage && (
-            <div className="Form__authError">
-              {errorMessage}
-              <IonIcon
-                icon={closeOutline}
-                onClick={() => setErrorMessage(null)}
-              />
-            </div>
-          )}
+    <Form
+      title="Sign Up"
+      form={<FormRegister />}
+      accountHelpLeftText="Already have an account?"
+      accountHelpLeftLink="/register"
+      accountHelpRightText="Signin"
+      accountHelpRightLink="/register"
+    />
 
-          <Formik
-            initialValues={{
-              firstName: "",
-              lastName: "",
-              email: "",
-              phone: "",
-              password: "",
-            }}
-            validationSchema={Yup.object({
-              firstName: firstNameValidation,
-              lastName: lastNameValidation,
-              email: emailValidation,
-              phone: phoneValidation,
-              password: passwordValidation,
-            })}
-            onSubmit={(values, actions) => {
-              const vals = { ...values };
-              actions.resetForm();
-              register(vals);
-            }}
-          >
-            {(formik) => (
-              <form onSubmit={formik.handleSubmit}>
-                <FormInput
-                  label="First Name"
-                  name="firstName"
-                  type="text"
-                  placeholder="Jane"
-                />
-                <FormInput
-                  label="Last Name"
-                  name="lastName"
-                  type="text"
-                  placeholder="First Name"
-                />
-                <div className="Form__inputGroup">
-                  <FormInput
-                    label="Email"
-                    name="email"
-                    type="email"
-                    placeholder="Email"
-                  />
-                  <FormInput
-                    label="Phone"
-                    name="phone"
-                    type="text"
-                    placeholder="Phone"
-                  />
-                  <FormInput
-                    label="Password"
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                  />
-                </div>
-
-                <IonButton type="submit">Register</IonButton>
-              </form>
-            )}
-          </Formik>
-
-          <div className="Form__accountHelp">
-            <p>
-              Have an account?{" "}
-              <Link to="/login" className="Form__accountHelp--right">
-                Sign In
-              </Link>
-            </p>
-          </div>
-        </div>
-      </IonContent>
-    </IonPage>
+    // <div className="Form__accountHelp">
+    //   <p>
+    //     Have an account?{" "}
+    //     <Link to="/login" className="Form__accountHelp--right">
+    //       Sign In
+    //     </Link>
+    //   </p>
+    // </div>
   );
 }
