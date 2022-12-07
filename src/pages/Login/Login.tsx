c56bfd903fbc29db3cff388dd72ecad8f55e82f0
Colin Boardway <cmboardway@gmail.com>
2022-12-07 14:02:18 -0600

    organized account help section

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,16 +2,23 @@ import Form from "components/Form/Form";
 import FormLogin from "components/Form/FormLogin";
 
 export default function Login() {
+  const accountHelpLeft = {
+    text: "Forgot password?",
+    link: "/forgot-password",
+  };
+
+  const accountHelpRight = {
+    text: "Create account",
+    link: "/register",
+  };
   return (
     <Form
       title="Login"
       subtitle="Hi there! Welcome to El Capitan."
       form={<FormLogin />}
       isSocialLoginShown={true}
-      accountHelpLeftText="Forgot password?"
-      accountHelpLeftLink="/forgot-password"
-      accountHelpRightText="Create account"
-      accountHelpRightLink="/register"
+      accountHelpLeft={accountHelpLeft}
+      accountHelpRight={accountHelpRight}
     />
   );
 }
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -2,23 +2,17 @@ import Form from "components/Form/Form";
 import FormRegister from "components/Form/FormRegister";
 
 export default function Register() {
+  const accountHelpCenter = {
+    text: "Already have an account?",
+    link: "/login",
+    linkText: "Sign In",
+  };
+
   return (
     <Form
       title="Sign Up"
       form={<FormRegister />}
-      accountHelpLeftText="Already have an account?"
-      accountHelpLeftLink="/register"
-      accountHelpRightText="Signin"
-      accountHelpRightLink="/register"
+      accountHelpCenter={accountHelpCenter}
     />
-
-    // <div className="Form__accountHelp">
-    //   <p>
-    //     Have an account?{" "}
-    //     <Link to="/login" className="Form__accountHelp--right">
-    //       Sign In
-    //     </Link>
-    //   </p>
-    // </div>
   );
 }
