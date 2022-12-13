3cce8ffc27c5b8e8903af2c6cb25a60a4f4d81b4
joshuabradley012 <joshuabradley012@gmail.com>
2022-12-13 12:24:40 -0800

    Moved shop page scss

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -5,9 +5,6 @@ import PageTemplate from "components/PageTemplate/PageTemplate";
 // consts
 import { products } from "pages/Shop/consts";
 
-// styles
-import "./Shop.scss";
-
 export default function Shop() {
   return (
     <PageTemplate title="Shop" className="Shop">
