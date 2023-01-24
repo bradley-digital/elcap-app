1192984200b6f597b5b834909fff2a06ecd1a84e
Colin Boardway <cmboardway@gmail.com>
2023-01-24 13:06:56 -0600

    final cals and cleanup

--- src/hooks/useChartData.ts
+++ src/hooks/useChartData.ts
@@ -1,67 +1,73 @@
-// import tempData from "components/DashboardOverview/tempData";
+import tempData from "components/DashboardOverview/tempData";
 
 export default function useChartData(
   year: number,
   selectedTransactionType: string
 ) {
   let initialBalance = 0;
-  // const currentBalance = 1000000.86;
-  const currentBalance = 15;
+  const currentBalance = 1000000.86;
+  // const currentBalance = 15;
   let transactionsYear: string | number;
-  const transactionsData: Array<number> = [];
+  const transactionData: Array<number> = [];
   const chartLabels: Array<string> = [];
-  const label = selectedTransactionType === "all" ? "Balance" : "Transactions";
+  const dataLabel =
+    selectedTransactionType === "all" ? "Balance" : "Transactions";
+  const transactionTypeMap = {
+    C: "Credit",
+    D: "Debit",
+    F: "Float",
+    M: "Miscellaneous Service Charge",
+    X: "Reversed",
+  };
 
-  const tempData = [
-    { transactionAmount: 2, transactionType: "C", postingDate: "03/01/2020" },
-    { transactionAmount: 10, transactionType: "D", postingDate: "03/02/2020" },
-    { transactionAmount: 3, transactionType: "C", postingDate: "03/03/2020" },
+  // const tempData = [
+  //   { transactionAmount: 2, transactionType: "C", postingDate: "03/01/2020" },
+  //   { transactionAmount: 10, transactionType: "D", postingDate: "03/02/2020" },
+  //   { transactionAmount: 3, transactionType: "C", postingDate: "03/03/2020" },
 
-    { transactionAmount: 2, transactionType: "D", postingDate: "03/04/2021" },
-    { transactionAmount: 3, transactionType: "C", postingDate: "03/05/2021" },
-    { transactionAmount: 4, transactionType: "D", postingDate: "03/06/2021" },
+  //   { transactionAmount: 2, transactionType: "D", postingDate: "03/04/2021" },
+  //   { transactionAmount: 3, transactionType: "C", postingDate: "03/05/2021" },
+  //   { transactionAmount: 4, transactionType: "D", postingDate: "03/06/2021" },
 
-    { transactionAmount: 2, transactionType: "D", postingDate: "03/04/2022" },
-    { transactionAmount: 10, transactionType: "C", postingDate: "03/05/2022" },
-    { transactionAmount: 7, transactionType: "D", postingDate: "03/06/2022" },
-  ];
+  //   { transactionAmount: 2, transactionType: "D", postingDate: "03/04/2022" },
+  //   { transactionAmount: 10, transactionType: "C", postingDate: "03/05/2022" },
+  //   { transactionAmount: 7, transactionType: "D", postingDate: "03/06/2022" },
+  // ];
 
   tempData.sort(
     (a, b) =>
       new Date(a.postingDate).getTime() - new Date(b.postingDate).getTime()
   );
 
-  function getInitialAmount(data, selectedYearBalance) {
+  function getInitialBalance(data, currentBalance) {
     data.forEach(({ transactionAmount, transactionType }) => {
+      const convertedTransactionAmount = Number(transactionAmount);
+
       switch (transactionType) {
         case "C":
-          selectedYearBalance += Number(transactionAmount);
+          currentBalance += convertedTransactionAmount;
           break;
         case "D":
-          selectedYearBalance -= Number(transactionAmount);
+          currentBalance -= convertedTransactionAmount;
           break;
         case "F":
-          selectedYearBalance += Number(transactionAmount); // validate
+          currentBalance += convertedTransactionAmount; // validate
           break;
         case "M":
-          selectedYearBalance -= Number(transactionAmount);
+          currentBalance -= convertedTransactionAmount;
           break;
         case "X":
-          selectedYearBalance += Number(transactionAmount); // validate
+          currentBalance += convertedTransactionAmount; // validate
           break;
         default:
           break;
       }
     });
-    return selectedYearBalance;
+    return currentBalance;
   }
 
   const transactionYears = new Set(
     tempData.map(({ postingDate }) => new Date(postingDate).getFullYear())
-  );
-
-  const firstTransactionYear = Math.min(
-    ...tempData.map(({ postingDate }) => new Date(postingDate).getFullYear())
   );
 
   const lastTransactionYear = Math.max(
@@ -72,37 +78,46 @@ export default function useChartData(
     tempData.map(({ transactionType }) => transactionType)
   );
 
-  const selectedYearData = tempData.filter(({ postingDate }) => {
-    const date = new Date(postingDate);
+  initialBalance = getInitialBalance(tempData, currentBalance);
 
-    if (year === 0) {
-      transactionsYear = "";
-      return date.getFullYear();
-    } else if (year === 1) {
-      transactionsYear = lastTransactionYear;
-      return date.getFullYear() === lastTransactionYear;
-    } else {
-      transactionsYear = year;
-      return date.getFullYear() === year;
-    }
-  });
-
-  const transactionTypeMap = {
-    C: "Credit",
-    D: "Debit",
-    F: "Float",
-    M: "Miscellaneous Service Charge",
-    X: "Reversed",
-  };
-
-  selectedYearData.forEach(
-    ({ transactionAmount, transactionType, postingDate }) => {
-      let amount = Number(transactionAmount);
+  let balanceAtTimeOfTransaction = initialBalance;
+  const transactionsWithBalance = tempData.map(
+    ({ transactionType, transactionAmount, postingDate }) => {
+      const convertedTransactionAmount = Number(transactionAmount);
 
       if (transactionType === "C") {
-        amount = amount * -1;
+        balanceAtTimeOfTransaction -= convertedTransactionAmount;
+      } else {
+        balanceAtTimeOfTransaction += convertedTransactionAmount;
       }
 
+      return {
+        transactionAmount: convertedTransactionAmount,
+        transactionType: transactionType,
+        postingDate: postingDate,
+        balanceAtTimeOfTransaction: balanceAtTimeOfTransaction,
+      };
+    }
+  );
+
+  const transactionsWithBalanceByYear = transactionsWithBalance.filter(
+    (transaction) => {
+      const date = new Date(transaction.postingDate);
+      if (year === 0) {
+        transactionsYear = "";
+        return date.getFullYear();
+      } else if (year === 1) {
+        transactionsYear = lastTransactionYear;
+        return date.getFullYear() === lastTransactionYear;
+      } else {
+        transactionsYear = year;
+        return date.getFullYear() === year;
+      }
+    }
+  );
+
+  transactionsWithBalanceByYear.forEach(
+    ({ transactionAmount, transactionType, postingDate }) => {
       const filterMap = {
         C: "C",
         D: "D",
@@ -118,41 +133,31 @@ export default function useChartData(
         return;
       }
 
-      transactionsData.push(amount);
-
       const date = new Date(postingDate);
       const options: any = { month: "short", day: "numeric", year: "numeric" };
       const shortDate = date.toLocaleDateString("en-US", options);
 
+      transactionData.push(transactionAmount);
       chartLabels.push(shortDate);
     }
   );
 
-  initialBalance = getInitialAmount(tempData, currentBalance);
-
-  const balanceData = transactionsData.reduce(
-    (acc, val) => {
-      const lastVal = acc[acc.length - 1];
-      const newVal = lastVal + val;
-      acc.push(newVal);
-      return acc;
-    },
-    [initialBalance]
+  const balanceData = transactionsWithBalanceByYear.map(
+    (x) => x.balanceAtTimeOfTransaction
   );
-  balanceData.shift();
+
+  const chartData =
+    selectedTransactionType === "all" ? balanceData : transactionData;
 
   chartLabels.sort(
     (a, b) => new Date(a).getFullYear() - new Date(b).getFullYear()
   );
 
-  const chartData =
-    selectedTransactionType === "all" ? balanceData : transactionsData;
-
   const data = {
     labels: chartLabels,
     datasets: [
       {
-        label: `${label}`,
+        label: `${dataLabel}`,
         data: chartData,
         borderColor: "green",
         backgroundColor: "rgba(102, 204, 153, 0.5)",
@@ -170,7 +175,7 @@ export default function useChartData(
       },
       title: {
         display: true,
-        text: `${transactionsYear} ${label}`,
+        text: `${transactionsYear} ${dataLabel}`,
       },
     },
     tooltips: {
@@ -186,7 +191,7 @@ export default function useChartData(
       y: {
         title: {
           display: true,
-          text: `${label}`,
+          text: `${dataLabel}`,
         },
       },
     },
@@ -200,5 +205,3 @@ export default function useChartData(
     transactionTypeMap,
   };
 }
-
-// useChartData(0, "all");
