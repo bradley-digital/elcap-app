11eb7563dc61dcf9da3f69d35864c9f2921d3646
Colin Boardway <cmboardway@gmail.com>
2023-02-03 09:52:56 -0600

    move port designation location

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgrPlugin()],
+  server: {
+    port: 3021,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
