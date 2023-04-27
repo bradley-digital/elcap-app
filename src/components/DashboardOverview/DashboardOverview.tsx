cdac9749db1144027964894b24417ce60204877b
joshuabradley012 <joshuabradley012@gmail.com>
2023-04-26 17:36:13 -0700

    Remove console log

--- src/components/DashboardOverview/DashboardOverview.tsx
+++ src/components/DashboardOverview/DashboardOverview.tsx
@@ -62,7 +62,6 @@ export default function DashboardOverview() {
     style: "currency",
     currency: "USD",
   });
-  console.log(typeof accountsCurrentBalanceTotal);
   const currentBalanceUSD = USD.format(accountsCurrentBalanceTotal);
   const timeRange = ["YTD", "MTD", "3M", "1Y", "3Y", "5Y", "Max"];
 
