aea1576529e7d588cbe6e1b801b91ef948fc2819
joshuabradley012 <joshuabradley012@gmail.com>
2023-05-24 15:52:59 -0700

    Added document list

--- src/pages/Onboarding/Onboarding.tsx
+++ src/pages/Onboarding/Onboarding.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import useUser from "hooks/useUser";
 
 // components
@@ -6,28 +7,96 @@ import {
   IonCol,
   IonContent,
   IonGrid,
+  IonIcon,
+  IonItem,
+  IonList,
   IonPage,
   IonRow,
+  IonSpinner,
   IonText,
 } from "@ionic/react";
+import {
+  checkmarkCircle,
+  closeCircle,
+  ellipsisHorizontalCircle,
+} from "ionicons/icons";
 import Logo from "components/Logo/Logo";
 import FormAccount from "components/FormAccount/FormAccount";
 
 // hooks
 import useUserDocfox from "hooks/useUserDocfox";
+import useAuth from "hooks/useAuth";
 
 import "./Onboarding.scss";
+
+const statusIcons = {
+  "approved": <IonIcon icon={checkmarkCircle} />,
+  "pending_upload": <IonIcon icon={closeCircle} />,
+  "rejected": <IonIcon icon={closeCircle} />,
+  "uploaded": <IonIcon icon={ellipsisHorizontalCircle} />,
+};
+
+const statusMessages = {
+  "approved": "Approved",
+  "pending_upload": "Not submitted",
+  "rejected": "Not approved",
+  "uploaded": "Under review",
+};
+
+const statusColors = {
+  "approved": "c-primary",
+  "pending_upload": "c-danger",
+  "rejected": "c-danger",
+  "uploaded": "",
+};
 
 type Props = {
   stage: string;
 };
 
 export default function Onboarding({ stage }: Props) {
+  const [documents, setDocuments] = useState<unkown[]>([]);
+  const [documentCount, setDocumentCount] = useState(0);
+  const { authApi } = useAuth();
   const { profile, updateUser } = useUser();
   const {
     dataRequirements,
     invitationLink,
   } = useUserDocfox();
+
+  useEffect(() => {
+    async function getDocuments() {
+      const newDocuments = [];
+      let newDocumentCount = 0;
+      let approvedDocumentCount = 0;
+      if (typeof dataRequirements !== "undefined") {
+        newDocumentCount = dataRequirements.data?.length || 0;
+        for (const dataRequirement of dataRequirements.data) {
+          const evidenceLink = dataRequirement?.relationships?.active_evidence_submission?.links?.related;
+          if (!evidenceLink) continue;
+          const evidenceId = evidenceLink.split("/").pop();
+          if (!evidenceId) continue;
+          const { data: evidenceSubmission } =
+            await authApi.get(`/docfox/evidence-submission?evidenceId=${evidenceId}`);
+          const status = evidenceSubmission?.data?.attributes?.status
+          const doc = {
+            name: dataRequirement?.attributes?.name,
+            status,
+          };
+          if (status === "approved") {
+            approvedDocumentCount++;
+          }
+          newDocuments.push(doc);
+        }
+      }
+      setDocuments(newDocuments);
+      setDocumentCount(newDocumentCount);
+      if (documentCount > 0 && documentCount === approvedDocumentCount) {
+        updateUser({ onboardingStage: "PENDING" });
+      }
+    }
+    getDocuments();
+  }, [dataRequirements]);
 
   if (typeof profile === "undefined") return null;
 
@@ -66,20 +135,36 @@ export default function Onboarding({ stage }: Props) {
       </div>
     );
   } else if (stage === "KYC") {
-    let documentCount = 0;
-    if (typeof dataRequirements !== "undefined") {
-      documentCount = dataRequirements.data?.length || 0;
+    if (documentCount > 0) {
+      content = (
+        <div className="t-center">
+          <IonText>
+            <p className="brow">Step 2</p>
+            <h1>New account documents</h1>
+            <p>{documentCount} documents required</p>
+          </IonText>
+          <div className="Onboarding__documents">
+            {documents.map(({ name, status }) => (
+              <div key={name} className="Onboarding__document">
+                <span>{name}</span>
+                <span className={`Onboarding__documentStatus ${statusColors[status]}`}>{statusIcons[status] || null} {statusMessages[status] || ""}</span>
+              </div>
+            ))}
+          </div>
+          <IonButton href={invitationLink} target="_blank">Upload documents</IonButton>
+        </div>
+      );
+    } else {
+      content = (
+        <div className="t-center">
+          <IonText>
+            <p className="brow">Step 2</p>
+            <h1>New account documents</h1>
+          </IonText>
+          <IonSpinner name="crescent" />
+        </div>
+      );
     }
-    content = (
-      <div className="t-center">
-        <IonText>
-          <p className="brow">Step 2</p>
-          <h1>New account documents</h1>
-          <p>{documentCount} documents required.</p>
-        </IonText>
-        <IonButton href={invitationLink} target="_blank">Upload documents</IonButton>
-      </div>
-    );
   } else if (stage === "PENDING") {
     content = (
       <div className="t-center">
