b6e0e57a02a4c1da6259dafbbe2ef86581764cc1
Vann Bryan Galleta <vannbryan22@gmail.com>
2023-07-21 01:35:20 +0800

    fix: resolved comments

--- src/components/PageTemplate/PageTemplate.tsx
+++ src/components/PageTemplate/PageTemplate.tsx
@@ -1,21 +1,16 @@
 import { type ReactNode } from "react";
-
 import {
-  IonBadge,
-  IonButton,
   IonButtons,
   IonContent,
   IonHeader,
-  IonIcon,
   IonMenuButton,
   IonPage,
   IonTitle,
   IonToolbar,
   useIonRouter,
 } from "@ionic/react";
-import { notificationsOutline } from "ionicons/icons";
-import { useNotification } from "hooks/useNotification";
 import "./PageTemplate.scss";
+import NotificationButton from "components/NotificationButton/NotificationButton";
 
 type Props = {
   children: ReactNode;
@@ -31,9 +26,6 @@ export default function PageTemplate({
   ...rest
 }: Props) {
   const router = useIonRouter();
-  const { unViewedCountIsSucces, unviewedNotificationsCount } = useNotification(
-    {}
-  );
   return (
     <IonPage {...rest} className="PageTemplate">
       <IonHeader>
@@ -46,27 +38,7 @@ export default function PageTemplate({
           <IonTitle>{title}</IonTitle>
 
           {router.routeInfo.pathname !== "/notifications" && (
-            <IonButtons slot="end">
-              <IonButton
-                onClick={() => {
-                  router.push("/notifications");
-                }}
-                color="dark"
-                className="PageTemplate__notification--button"
-                slot="end"
-              >
-                <IonIcon
-                  icon={notificationsOutline}
-                  size="large"
-                  className="PageTemplate__notification--bell"
-                />
-                {unViewedCountIsSucces && unviewedNotificationsCount > 0 && (
-                  <IonBadge slot="start" color="tertiary">
-                    {unviewedNotificationsCount}
-                  </IonBadge>
-                )}
-              </IonButton>
-            </IonButtons>
+            <NotificationButton />
           )}
         </IonToolbar>
       </IonHeader>
