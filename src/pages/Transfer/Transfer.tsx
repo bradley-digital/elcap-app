0549c8f3cef400151fdb526c633ffe2a16977e9c
Vann Bryan Galleta <vannbryan22@gmail.com>
2023-08-25 23:26:27 +0800

    refactor:various naming conventions

--- src/pages/Transfer/Transfer.tsx
+++ src/pages/Transfer/Transfer.tsx
@@ -20,19 +20,19 @@ export default function Profile(routeProps: RouteComponentProps) {
       id: 2,
       icon: swapHorizontal,
       href: `${match.url}/account`,
-      label: "Account transfer",
+      label: "Between Account Transfer",
     },
     {
       id: 3,
       icon: exit,
       href: `${match.url}/external`,
-      label: "External transfer",
+      label: "External Transfer",
     },
   ];
 
   return (
     <SplitPaneTemplate
-      title="Money movement"
+      title="Money Movement"
       menuId="transfer"
       menuLinks={menuLinks}
     >
