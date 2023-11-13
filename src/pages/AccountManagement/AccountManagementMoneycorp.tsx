c8c3d07bdced9a2ee00650408cf961731b541315
Vann Bryan Galleta <vannbryan22@gmail.com>
2023-11-14 02:54:19 +0800

    udpate:removed modal for creation

--- src/pages/AccountManagement/AccountManagementMoneycorp.tsx
+++ src/pages/AccountManagement/AccountManagementMoneycorp.tsx
@@ -1,31 +1,14 @@
-import {
-  IonCol,
-  IonFab,
-  IonFabButton,
-  IonGrid,
-  IonIcon,
-  IonRow,
-} from "@ionic/react";
-import { useAtom } from "jotai";
-import { isOpenAtom } from "atoms/moneycorpModal";
+import { IonCol, IonGrid, IonRow } from "@ionic/react";
 import PageTemplate from "components/PageTemplate/PageTemplate";
 
 // hooks
 import useMoneycorpAccount from "hooks/useMoneycorpAccount";
 
 // icons
-import { add } from "ionicons/icons";
 import MoneycorpList from "components/MoneycorpList/MoneycorpList";
-import MoneycorpListModal from "components/MoneycorpListModal/MoneycorpListModal";
 
 export default function AccountManagementMoneycorp() {
   const { accountsIsSuccess, accounts } = useMoneycorpAccount();
-
-  const [, setIsOpen] = useAtom(isOpenAtom);
-
-  function openModal() {
-    setIsOpen(true);
-  }
 
   if (accountsIsSuccess && typeof accounts !== "undefined") {
     return (
@@ -34,15 +17,9 @@ export default function AccountManagementMoneycorp() {
           <IonRow className="ion-justify-content-center">
             <IonCol size-md="8" size-lg="6">
               <MoneycorpList />
-              <MoneycorpListModal />
             </IonCol>
           </IonRow>
         </IonGrid>
-        <IonFab vertical="bottom" horizontal="end" slot="fixed">
-          <IonFabButton onClick={openModal}>
-            <IonIcon icon={add} />
-          </IonFabButton>
-        </IonFab>
       </PageTemplate>
     );
   }
